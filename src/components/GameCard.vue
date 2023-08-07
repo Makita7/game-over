e0ccab0c7328febf19bf78ecd43027e06d668d1a
sofia-makita <smakita@iadb.org>
2023-08-07 15:51:26 -0300

    link for details finished

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -19,95 +19,102 @@
 </script>
 
 <template>
-    <div class="card mb-6 fadingIn ms-1">
-        <div class="mt-2 mr-3 align-last pl-2">
-            <div class="label ml-3">
-                <p class="text-capitalize font-italic">saved to List_Name</p>
+    <div class="  fadingIn ms-1 mb-6 ">
+        <div class="card">
+            <div class="mt-2 mr-3 align-last pl-2">
+                <div class="label ml-3">
+                    <p class="text-capitalize font-italic">saved to List_Name</p>
+                </div>
+                <v-spacer/>
+                <!-- <v-btn variant="text" class="iconSize" icon="mdi-close-circle" /> -->
+                <v-btn variant="text" class="iconSize" icon="mdi-plus-circle" />
             </div>
-            <v-spacer/>
-            <!-- <v-btn variant="text" class="iconSize" icon="mdi-close-circle" /> -->
-            <v-btn variant="text" class="iconSize" icon="mdi-plus-circle" />
+
+            <div class="mr-auto ml-auto mt-2 imgContainer">
+                <img style="width:100%;" :src="img" :alt="title" />
+
+            </div>
+            <div>
+                <div class="wrap ml-3 mt-3">
+                    <v-chip
+                        v-for="g in genres.slice(0,4)"
+                        :key="g.id"
+                        color="genre"
+                        size="small"
+                        variant="elevated"
+                        class="text-capitalize ma-1 pill"
+                    >
+                        {{ g.name }}
+                    </v-chip>
+                    <v-chip
+                        v-if="genres > 4"
+                        variant="elevated"
+                        size="small"
+                        color="platform"
+                        class="text-capitalize ma-1 pill"
+                    >
+                        ...
+                    </v-chip>
+                </div>
+
+                <v-divider class="mt-2 mb-2"/>
+
+                <div class="wrap ml-3" >
+                    <v-chip
+                        v-for="p in platforms.slice(0,4)"
+                        :key="p.platform.id"
+                        variant="elevated"
+                        size="small"
+                        color="platform"
+                        class="text-capitalize ma-1 pill"
+                    >
+                        {{ p.platform.name }}
+                    </v-chip>
+                    <v-chip
+                        v-if="platforms > 4"
+                        variant="elevated"
+                        size="small"
+                        color="platform"
+                        class="text-capitalize ma-1 pill"
+                    >
+                        ...
+                    </v-chip>
+
+                </div>
+
+                <v-divider class="mt-2 mb-2"/>
+
+                <div class="wrap mt-1 ml-3 mb-3" >
+                    <v-chip
+                        v-for="t in tags.slice(0,3)"
+                        :key="t.id"
+                        variant="elevated"
+                        size="small"
+                        color="tag"
+                        class="text-capitalize ma-1 pill"
+                    >
+                        {{ t.name }}
+                    </v-chip>
+                    <v-chip
+                        v-if="tags.length > 3"
+                        variant="elevated"
+                        size="small"
+                        color="tag"
+                        class="text-capitalize ma-1 pill"
+                    >
+                        ...
+                    </v-chip>
+
+                </div>
+            </div>
         </div>
-
-        <div class="mr-auto ml-auto mt-2 imgContainer">
-            <img style="width:100%;" :src="img" :alt="title" />
-
-        </div>
-        <div>
-            <div class="wrap ml-3 mt-3">
-                <v-chip
-                    v-for="g in genres.slice(0,4)"
-                    :key="g.id"
-                    color="genre"
-                    size="small"
-                    variant="elevated"
-                    class="text-capitalize ma-1 pill"
-                >
-                    {{ g.name }}
-                </v-chip>
-                <v-chip
-                    v-if="genres > 4"
-                    variant="elevated"
-                    size="small"
-                    color="platform"
-                    class="text-capitalize ma-1 pill"
-                >
-                    ...
-                </v-chip>
-            </div>
-
-            <v-divider class="mt-2 mb-2"/>
-
-            <div class="wrap ml-3" >
-                <v-chip
-                    v-for="p in platforms.slice(0,4)"
-                    :key="p.platform.id"
-                    variant="elevated"
-                    size="small"
-                    color="platform"
-                    class="text-capitalize ma-1 pill"
-                >
-                    {{ p.platform.name }}
-                </v-chip>
-                <v-chip
-                    v-if="platforms > 4"
-                    variant="elevated"
-                    size="small"
-                    color="platform"
-                    class="text-capitalize ma-1 pill"
-                >
-                    ...
-                </v-chip>
-
-            </div>
-
-            <v-divider class="mt-2 mb-2"/>
-
-            <div class="wrap mt-1 ml-3 mb-3" >
-                <v-chip
-                    v-for="t in tags.slice(0,3)"
-                    :key="t.id"
-                    variant="elevated"
-                    size="small"
-                    color="tag"
-                    class="text-capitalize ma-1 pill"
-                >
-                    {{ t.name }}
-                </v-chip>
-                <v-chip
-                    v-if="tags.length > 3"
-                    variant="elevated"
-                    size="small"
-                    color="tag"
-                    class="text-capitalize ma-1 pill"
-                >
-                    ...
-                </v-chip>
-
-            </div>
-        </div>
+        <RouterLink :to="`/detail/${id}`" class="d-flex justify-end mt-1" style="text-decoration: none !important;">
+            More Details
+            <v-icon aria-hidden="false" class="ml-2">
+                mdi-eye
+            </v-icon>
+        </RouterLink>
     </div>
-        <RouterLink :to="`/detail/${id}`">Link</RouterLink>
 </template>
 
 <style scoped lang="scss">
@@ -137,6 +144,7 @@
             margin-bottom: auto;
             font-weight: 500;
         }
+
         .v-btn--icon.v-btn--size-default {
             --v-btn-size: 1.2rem;
         }
